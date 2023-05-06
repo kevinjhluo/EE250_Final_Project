ae48b6a41169dc9efd2d8e7ca546302d2842a8a7
kevinjhluo <luok@usc.edu>
2023-05-05 18:44:58 -0700

    update

--- button.py
+++ button.py
@@ -30,10 +30,9 @@ def decode_morse_code(message):
                 morse_code += '.' 
             num_of_ones = 0 
     print(morse_code)
-    for code in morse_code.split():
-        for letter, code in MORSE_CODE_DICT.items():
-            if code == morse_code:
-                decoded_message += letter
+    for code in morse_code.split(" "):
+        letter = list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(code)]
+        decoded_message += letter
     return decoded_message
 
 # Define the main function
